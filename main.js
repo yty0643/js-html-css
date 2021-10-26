26a275b3bfc1b3568c6bb159de8c93d1f5f419ff
yty0643 <80657819+yty0643@users.noreply.github.com>
2021-10-26 20:57:19 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
@@ -8,4 +8,19 @@ document.addEventListener('scroll', ()=>{
     }else{
         navbar.classList.remove('navbar__dark');
     }
-})
+});
+
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
