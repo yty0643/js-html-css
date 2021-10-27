5242ae0ba1ccd6f78b036697b9d5a41467251b44
yty0643 <80657819+yty0643@users.noreply.github.com>
2021-10-27 13:39:32 +0900

    JS: arrow up

--- main.js
+++ main.js
@@ -12,8 +12,15 @@ document.addEventListener('scroll', ()=>{
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click',(event)=>{
+const contactBtn = document.querySelector('.home__contact');
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
     
+};
+
+navbarMenu.addEventListener('click',(event)=>{
     const target = event.target;
     const link = target.dataset.link;
 
@@ -23,22 +30,30 @@ navbarMenu.addEventListener('click',(event)=>{
     scrollIntoView(link);
 });
 
-const contactBtn = document.querySelector('.home__contact');
 contactBtn.addEventListener('click',()=>{
     scrollIntoView('#contact');
 });
 
-function scrollIntoView(selector){
-    const scrollTo = docuement.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
-};
+// Hide the contents when scrolling.
+
+const homeHeight = document.querySelector('#home').getBoundingClientRect().height;
+const homeContent = document.querySelector('.home__container');
 
 document.addEventListener('scroll', ()=>{
-    const homeHeight = document.querySelector('#home').getBoundingClientRect().height;
-    const homeContent = document.querySelector('.home__container');
     homeContent.style.opacity = 1 - scrollY/homeHeight;
-    
-    
-})
+});
 
+// arrow up button
 
+const arrowUpBtn = document.querySelector('.arrow-up');
+arrowUpBtn.addEventListener('click',()=>{
+    scrollIntoView('#home');
+});
+
+document.addEventListener('scroll', ()=>{
+    if(scrollY > homeHeight/2){
+        arrowUpBtn.classList.add('visible');
+    }else{
+        arrowUpBtn.classList.remove('visible');
+    }
+});
